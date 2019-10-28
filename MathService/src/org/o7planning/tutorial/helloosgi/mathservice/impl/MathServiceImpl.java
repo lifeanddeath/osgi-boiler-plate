16dc9d2221e77338d88c1854bac6443b99ca50df
Ahmet Eroglu <Ahmet.Eroglu@ricoh-europe.com>
2019-10-28 17:04:05 +0100

    initial commit

---
+++ MathService/src/org/o7planning/tutorial/helloosgi/mathservice/impl/MathServiceImpl.java
@@ -0,0 +1,13 @@
+package org.o7planning.tutorial.helloosgi.mathservice.impl;
+
+import org.o7planning.tutorial.helloosgi.mathservice.MathService;
+
+public class MathServiceImpl implements MathService{
+
+	@Override
+	public int sum(int a, int b) {
+		// TODO Auto-generated method stub
+		return a + b;
+	}
+
+}
---
+++ MathService/src/org/o7planning/tutorial/helloosgi/utils/MathUtils.java
@@ -0,0 +1,9 @@
+package org.o7planning.tutorial.helloosgi.utils;
+
+public class MathUtils {
+	
+	public static int minus(int a, int b) {
+		return a-b;
+	}
+
+}
